7327c745def12a84689b37dba363b8924fbd925d
Princeratheee <rathi.prince2@gmail.com>
2025-10-20 12:14:38 +0530

    Remove data correction notice from CompanyStatisticsChart component for cleaner UI

--- client/src/Components/marketplace/manager/CompanyStatisticsChart.jsx
+++ client/src/Components/marketplace/manager/CompanyStatisticsChart.jsx
@@ -474,40 +474,7 @@ export default function CompanyStatistics() {
         </div>
       </div>
 
-      {/* Data Correction Notice */}
-      {dataCorrections.length > 0 && (
-        <div className="mb-6 p-1 bg-yellow-50 border border-yellow-200 rounded-lg">
-          <div className="flex items-start">
-            <div className="flex-shrink-0">
-              <svg className="h-3 w-3 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-              </svg>
-            </div>
-            <div className="ml-3">
-              <h3 className="text-sm font-medium text-yellow-800">Data Validation Applied</h3>
-              <div className="mt-2 text-sm text-yellow-700">
-                <p>Some data has been automatically corrected to ensure logical consistency:</p>
-                <ul className="mt-1 list-disc list-inside">
-                  {dataCorrections.map((correction, index) => (
-                    <li key={index}>
-                      {correction.type === 'monthly' 
-                        ? `${correction.month}: Limited picks from ${correction.originalPicks} to ${correction.correctedPicks} (cannot exceed jobs posted)`
-                        : correction.type === 'cumulative'
-                        ? `${correction.month}: Reduced picks from ${correction.originalPicks} to ${correction.correctedPicks} (maintaining cumulative logic)`
-                        : correction.type === 'line-crossing'
-                        ? `${correction.month}: Prevented line crossing by reducing picks from ${correction.originalPicks} to ${correction.correctedPicks}`
-                        : `${correction.month}: Scaled picks from ${correction.originalPicks} to ${correction.correctedPicks} (${Math.round(correction.scaleFactor * 100)}% of original)`
-                      }
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-
-        <div className="relative">
+      <div className="relative">
           <svg 
             width={chartWidth} 
             height={chartHeight} 
