8c715fdab44c50d9ee59a33cd8bb480caeacf49f
princeratheee <rathi.prince2@gmail.com>
2026-04-13 05:01:25 +0530

    style: update About page color scheme and swap component layout for partners section

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 import ZepAboutNav from '../Components/ZepAboutNew/ZepAboutNav';
 import ZepAboutHero from '../Components/ZepAboutNew/ZepAboutHero';
 import ZepAboutOSSection from '../Components/ZepAboutNew/ZepAboutOSSection';
+import ZepAboutPartners from '../Components/ZepAboutNew/ZepAboutPartners';
 import ZepAboutValues from '../Components/ZepAboutNew/ZepAboutValues';
 import ZepAboutFAQ from '../Components/ZepAboutNew/ZepAboutFAQ';
 import ZepAboutBeyondCTA from '../Components/ZepAboutNew/ZepAboutBeyondCTA';
 import ZepAboutFooter from '../Components/ZepAboutNew/ZepAboutFooter';
 import '../styles/ZepAbout.css';
+import LandingBeyondCTA from '../Components/landing/LandingBeyondCTA';
 
 const About = () => {
     return (
@@ -14,10 +16,12 @@ const About = () => {
             <ZepAboutNav />
             <ZepAboutHero />
             <ZepAboutOSSection />
-            <ZepAboutValues />
+            <ZepAboutPartners />
+            {/* <ZepAboutValues /> */}
             <ZepAboutFAQ />
-            <ZepAboutBeyondCTA />
-            <ZepAboutFooter />
+            {/* <ZepAboutBeyondCTA /> */}
+            {/* <ZepAboutFooter /> */}
+            <LandingBeyondCTA/>
         </Fragment>
     );
 };
